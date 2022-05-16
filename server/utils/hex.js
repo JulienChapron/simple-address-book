de3951203327326117aeea5024ca975d186c853e
Julien Chapron <60253712+JulienChapron@users.noreply.github.com>
2022-05-16 17:28:29 +0200

    Update hex.js

--- server/utils/hex.js
+++ server/utils/hex.js
@@ -1,1 +1,8 @@
 
+export function hexToBytesArray(hex: any) {
+  const bytes = [];
+  for (let c = 0; c < hex.length; c += 2) {
+    bytes.push(parseInt(hex.substr(c, 2), 16));
+  }
+  return bytes;
+}
